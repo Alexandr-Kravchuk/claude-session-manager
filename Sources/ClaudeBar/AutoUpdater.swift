1c8a25c89070011a93e0c378e35ccd300f8bcfdc
Alex Kravchuk <12455901+Alexandr-Kravchuk@users.noreply.github.com>
2026-07-22 17:21:07 +0200

    Bump to 2.10.0

--- Sources/ClaudeBar/AutoUpdater.swift
+++ Sources/ClaudeBar/AutoUpdater.swift
@@ -19,7 +19,7 @@ enum UpdateError: LocalizedError {
 
 @MainActor
 final class AutoUpdater: ObservableObject {
-    static let currentVersion = "2.9.0"
+    static let currentVersion = "2.10.0"
     static let autoUpdateKey = "com.claudebar.autoUpdate"
 
     @Published var latestVersion: String?
